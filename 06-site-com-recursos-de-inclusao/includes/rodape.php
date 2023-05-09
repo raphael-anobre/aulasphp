e86f18189b67784927dd915060d87ba4995ce5df
raphael-anobre <raphael_anobre@hotmail.com>
2023-05-09 17:25:17 -0300

    Site com include

---
+++ 06-site-com-recursos-de-inclusao/includes/rodape.php
@@ -0,0 +1,11 @@
+</main>
+
+    <footer>
+        <p>Desenvolvido por <b>Raphael</b></p>
+        <p>Programador web - 2023</p>
+    </footer>
+
+
+</body>
+
+</html>
---
+++ 06-site-com-recursos-de-inclusao/index.php
@@ -0,0 +1,8 @@
+        <?php include "includes/cabecalho.php"; ?>
+
+        <h2>Bem-vindo(a) ao site de
+            exemplo!</h2>
+        <p>Esta é a primeira página.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ab error laborum debitis, explicabo accusantium sit nesciunt itaque inventore quod totam odio qui mollitia, nemo quisquam repudiandae! Inventore, iusto molestiae!</p>
+    
+        <?php include "includes/rodape.php"; ?>
